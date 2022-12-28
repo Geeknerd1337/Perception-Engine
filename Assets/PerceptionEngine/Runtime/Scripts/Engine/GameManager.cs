8937b4dfb98e58233af0d20252d128124fdee9e0
Geeknerd1337 <geeknerd1337@gmail.com>
2022-12-28 15:02:07 -0600

    Perception Engine Game Class

---
+++ Assets/PerceptionEngine/Runtime/Scripts/Engine/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Perception.Engine
+{
+    /// <summary>
+    /// This is the primary class involved in managing the game. This controls the camera as well as any relevant services which it maintains.
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+
+    }
+
+}
