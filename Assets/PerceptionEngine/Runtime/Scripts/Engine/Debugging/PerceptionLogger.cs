5a77911573d37906b2e02e293887d0d45bcc6a2c
Geeknerd1337 <geeknerd1337@gmail.com>
2022-12-30 14:08:10 -0600

    Add Verbose Logging

---
+++ Assets/PerceptionEngine/Runtime/Scripts/Engine/Debugging/PerceptionLogger.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Object = UnityEngine.Object;
+
+namespace Perception.Engine
+{
+    public static class PerceptionLogger
+    {
+        private static void DoLog(Action<string, Object> LogFunction, string prefix, Object obj, params object[] msg)
+        {
+#if UNITY_EDITOR
+            LogFunction($"{prefix}[<color=lightblue>{obj.name}</color>]: {String.Join(";", msg)}", obj);
+#endif
+        }
+
+        public static void Log(this Object obj, params object[] msg)
+        {
+            DoLog(Debug.Log, "", obj, msg);
+        }
+
+        public static void LogError(this Object obj, params object[] msg)
+        {
+            DoLog(Debug.LogError, "<color=red><!></color>", obj, msg);
+        }
+
+        public static void LogWarning(this Object obj, params object[] msg)
+        {
+            DoLog(Debug.LogWarning, "⚠️", obj, msg);
+        }
+
+        public static void LogSuccess(this Object obj, params object[] msg)
+        {
+            DoLog(Debug.Log, "<color=green><!></color>", obj, msg);
+        }
+
+
+    }
+}
