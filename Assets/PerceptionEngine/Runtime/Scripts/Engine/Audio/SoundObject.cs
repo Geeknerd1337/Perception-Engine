02ed1f05869d15a5b5ad27c058a9aee86a8688d7
Geeknerd1337 <geeknerd1337@gmail.com>
2023-01-07 17:40:16 -0600

    Update SoundObject.cs

--- Assets/PerceptionEngine/Runtime/Scripts/Engine/Audio/SoundObject.cs
+++ Assets/PerceptionEngine/Runtime/Scripts/Engine/Audio/SoundObject.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 namespace Perception.Engine
 {
+    /// <summary>
+    /// This incredibly useful script is thanks to a bit of reading I've done on sound assets in unity from Baruchadi on github. You can see the original gist
+    /// this is based on over here:
+    /// https://gist.github.com/baruchadi/3c23caf609fa0f4bd349d9ea432eb9c4
+    /// </summary>
     [System.Serializable]
+    [CreateAssetMenu(fileName = "New Sound", menuName = "Perception/Audio/New Sound")]
     public class SoundObject : ScriptableObject
     {
+        /// <summary>
+        /// Semitones are a unit of measurement for pitch. This is the number of semitones that the pitch of the sound will be shifted by.
+        /// This is better than just randoming a pitch because it allows us to have a more consistent sound.
+        /// </summary>
         private static readonly float SEMITONES_TO_PITCH_CONVERSION_UNIT = 1.05946f;
+
 
 
     }
