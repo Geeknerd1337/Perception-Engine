b75d83b2cfe25015788a5c29f503e96b52d0a367
Geeknerd1337 <geeknerd1337@gmail.com>
2023-01-09 15:27:59 -0600

    Add ModifiablePropertyDrawer
    Added ModifiablePropertyDrawer so that properties inheriting from ModifiablePropertyAttribute can use the methods in PropertyModifierAttribute to have the ability to use multiple PropertyDrawers on a single field.

---
+++ Assets/PerceptionEngine/Editor/Property Drawers/ModifiablePropertyDrawer.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Perception.Engine;
+
+namespace Perception.Editor
+{
+    /// <summary>
+    /// Draws properties with the ModifiableProperty Attribute  calling the three methods of PropertyModifierAttribute.
+    /// This way you can effectively stack multiple property attributes on a single property.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(ModifiablePropertyAttribute), true)]
+    public class ModifiablePropertyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var modifiable = (ModifiablePropertyAttribute)attribute;
+            if (modifiable.modifiers == null)
+                modifiable.modifiers = fieldInfo.GetCustomAttributes(typeof(PropertyModifierAttribute), false)
+                .Cast<PropertyModifierAttribute>().OrderBy(s => s.order).ToList();
+
+            float height = ((ModifiablePropertyAttribute)attribute).GetPropertyHeight(property, label);
+            foreach (var attr in modifiable.modifiers)
+                height = attr.GetHeight(property, label, height);
+            return height;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var modifiable = (ModifiablePropertyAttribute)attribute;
+
+            bool visible = true;
+            foreach (var attr in modifiable.modifiers.AsEnumerable().Reverse())
+                visible = attr.BeforeGUI(ref position, property, label, visible);
+
+            if (visible)
+                modifiable.OnGUI(position, property, label);
+
+            foreach (var attr in modifiable.modifiers)
+                attr.AfterGUI(position, property, label);
+        }
+    }
+}
