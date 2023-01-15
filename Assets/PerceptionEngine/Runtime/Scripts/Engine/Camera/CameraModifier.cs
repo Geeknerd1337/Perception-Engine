3595809e89e642c7889b3fd0aa21ab92e8d9d4da
Geeknerd1337 <geeknerd1337@gmail.com>
2023-01-15 01:22:11 -0600

    Add CameraModifier to project
    Add camera modifier which acts as basis for of being able to manipulate cameras positions and rotations.

---
+++ Assets/PerceptionEngine/Runtime/Scripts/Engine/Camera/CameraModifier.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Perception.Engine
+{
+	public class CameraModifier : MonoBehaviour
+	{
+		public virtual void PostCameraSetup( ref Tripod.Setup camSetup )
+		{
+
+		}
+	}
+}
