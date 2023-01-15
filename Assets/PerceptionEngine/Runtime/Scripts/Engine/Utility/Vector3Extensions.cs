ca6e9084f839ad7ec4f4c6aacc55935280407d71
Geeknerd1337 <geeknerd1337@gmail.com>
2023-01-15 02:11:27 -0600

    Add Vector3Extension to project
    Add a series of helper methods for Vector3s

---
+++ Assets/PerceptionEngine/Runtime/Scripts/Engine/Utility/Vector3Extensions.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Perception.Engine{
+    public static class Vector3Extensions
+	{
+		// With
+
+		public static Vector3 WithX( this Vector3 vector, float value )
+		{
+			return new( value, vector.y, vector.z );
+		}
+
+		public static Vector3 WithY( this Vector3 vector, float value )
+		{
+			return new( vector.x, value, vector.z );
+		}
+
+		public static Vector3 WithZ( this Vector3 vector, float value )
+		{
+			return new( vector.x, vector.y, value );
+		}
+
+		// Flip
+
+		public static Vector3 FlipX( this Vector3 v )
+		{
+			return new( -v.x, v.y, v.z );
+		}
+
+		public static Vector3 FlipY( this Vector3 v )
+		{
+			return new( v.x, -v.y, v.z );
+		}
+
+		public static Vector3 FlipZ( this Vector3 v )
+		{
+			return new( v.x, v.y, -v.z );
+		}
+
+		public static Vector3 Approach( this Vector3 v, float length, float amount )
+		{
+			return v.normalized * length.Approach( length, amount );
+		}
+
+		// Lerp
+
+		public static Vector3 LerpTo( this Vector3 input, Vector3 b, float t )
+		{
+			return Vector3.Lerp( input, b, t );
+		}
+
+		public static Vector3 SlerpTo( this Vector3 input, Vector3 b, float t )
+		{
+			return Vector3.Slerp( input, b, t );
+		}
+
+		public static Vector3 ProjectileCalculateLead( Vector3 startPosition, Vector3 targetPosition, Vector3 targetVelocity, float velocity )
+		{
+			float distance = Vector3.Distance( startPosition, targetPosition );
+			float travelTime = distance / (velocity);
+			return (targetPosition + targetVelocity * travelTime);
+		}
+
+		public static float Dot( this Vector3 input, Vector3 b )
+		{
+			return Vector3.Dot( input, b );
+		}
+	}
+}
