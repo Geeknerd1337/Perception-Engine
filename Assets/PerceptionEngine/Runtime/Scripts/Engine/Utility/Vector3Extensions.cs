4c83d1dff9c3a0e9c5d8c8cd70c5dfaba14de7c1
Geeknerd1337 <geeknerd1337@gmail.com>
2023-01-15 02:12:34 -0600

    Add Vector3Extensions to project
    Add a series of helper functions to Vector3

--- Assets/PerceptionEngine/Runtime/Scripts/Engine/Utility/Vector3Extensions.cs
+++ Assets/PerceptionEngine/Runtime/Scripts/Engine/Utility/Vector3Extensions.cs
@@ -2,70 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Perception.Engine{
+namespace Perception.Engine
+{
     public static class Vector3Extensions
-	{
-		// With
+    {
+        // With
 
-		public static Vector3 WithX( this Vector3 vector, float value )
-		{
-			return new( value, vector.y, vector.z );
-		}
+        public static Vector3 WithX(this Vector3 vector, float value)
+        {
+            return new Vector3(value, vector.y, vector.z);
+        }
 
-		public static Vector3 WithY( this Vector3 vector, float value )
-		{
-			return new( vector.x, value, vector.z );
-		}
+        public static Vector3 WithY(this Vector3 vector, float value)
+        {
+            return new Vector3(vector.x, value, vector.z);
+        }
 
-		public static Vector3 WithZ( this Vector3 vector, float value )
-		{
-			return new( vector.x, vector.y, value );
-		}
+        public static Vector3 WithZ(this Vector3 vector, float value)
+        {
+            return new Vector3(vector.x, vector.y, value);
+        }
 
-		// Flip
+        // Flip
 
-		public static Vector3 FlipX( this Vector3 v )
-		{
-			return new( -v.x, v.y, v.z );
-		}
+        public static Vector3 FlipX(this Vector3 v)
+        {
+            return new Vector3(-v.x, v.y, v.z);
+        }
 
-		public static Vector3 FlipY( this Vector3 v )
-		{
-			return new( v.x, -v.y, v.z );
-		}
+        public static Vector3 FlipY(this Vector3 v)
+        {
+            return new Vector3(v.x, -v.y, v.z);
+        }
 
-		public static Vector3 FlipZ( this Vector3 v )
-		{
-			return new( v.x, v.y, -v.z );
-		}
+        public static Vector3 FlipZ(this Vector3 v)
+        {
+            return new Vector3(v.x, v.y, -v.z);
+        }
 
-		public static Vector3 Approach( this Vector3 v, float length, float amount )
-		{
-			return v.normalized * length.Approach( length, amount );
-		}
+        public static Vector3 Approach(this Vector3 v, float length, float amount)
+        {
+            return v.normalized * length.Approach(length, amount);
+        }
 
-		// Lerp
+        // Lerp
 
-		public static Vector3 LerpTo( this Vector3 input, Vector3 b, float t )
-		{
-			return Vector3.Lerp( input, b, t );
-		}
+        public static Vector3 LerpTo(this Vector3 input, Vector3 b, float t)
+        {
+            return Vector3.Lerp(input, b, t);
+        }
 
-		public static Vector3 SlerpTo( this Vector3 input, Vector3 b, float t )
-		{
-			return Vector3.Slerp( input, b, t );
-		}
+        public static Vector3 SlerpTo(this Vector3 input, Vector3 b, float t)
+        {
+            return Vector3.Slerp(input, b, t);
+        }
 
-		public static Vector3 ProjectileCalculateLead( Vector3 startPosition, Vector3 targetPosition, Vector3 targetVelocity, float velocity )
-		{
-			float distance = Vector3.Distance( startPosition, targetPosition );
-			float travelTime = distance / (velocity);
-			return (targetPosition + targetVelocity * travelTime);
-		}
+        public static Vector3 ProjectileCalculateLead(Vector3 startPosition, Vector3 targetPosition, Vector3 targetVelocity, float velocity)
+        {
+            float distance = Vector3.Distance(startPosition, targetPosition);
+            float travelTime = distance / (velocity);
+            return (targetPosition + targetVelocity * travelTime);
+        }
 
-		public static float Dot( this Vector3 input, Vector3 b )
-		{
-			return Vector3.Dot( input, b );
-		}
-	}
+        public static float Dot(this Vector3 input, Vector3 b)
+        {
+            return Vector3.Dot(input, b);
+        }
+    }
 }
