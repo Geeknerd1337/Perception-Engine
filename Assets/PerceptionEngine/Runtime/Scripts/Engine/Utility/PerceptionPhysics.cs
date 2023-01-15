30a0fc1bb03b00cd8d83d24e8ea1f42e5233315f
Geeknerd1337 <geeknerd1337@gmail.com>
2023-01-15 01:44:33 -0600

    Add PerceptionPhysics to project.
    Added physics helper which will form basis of controllers.

---
+++ Assets/PerceptionEngine/Runtime/Scripts/Engine/Utility/PerceptionPhysics.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Perception.Engine
+{
+    /// <summary>
+    /// Physics Helper
+    /// </summary>
+    public class PerceptionPhysics
+    {
+        public static int GroundLayerMask = LayerMask.GetMask(new string[] { "Default" });
+    }
+}
